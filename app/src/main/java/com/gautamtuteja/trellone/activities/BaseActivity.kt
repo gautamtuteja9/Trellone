29dad5bfebaffd27440cb2db49394c637207bff6
Gautam Tuteja <101208568+gautamtuteja9@users.noreply.github.com>
2024-08-14 16:22:57 +0530

    Add files via upload

--- app/src/main/java/com/gautamtuteja/trellone/activities/BaseActivity.kt
+++ app/src/main/java/com/gautamtuteja/trellone/activities/BaseActivity.kt
@@ -1,67 +1,83 @@
-package com.gautamtuteja.trellone.activities
+package com.projemanag.activities
 
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import com.gautamtuteja.trellone.R
-import com.gautamtuteja.trellone.databinding.ActivityBaseBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.projemanag.R
+import kotlinx.android.synthetic.main.dialog_progress.*
 
 open class BaseActivity : AppCompatActivity() {
 
     private var doubleBackToExitPressedOnce = false
 
-    private lateinit var mProgressDialog : Dialog
+    /**
+     * This is a progress dialog instance which we will initialize later on.
+     */
+    private lateinit var mProgressDialog: Dialog
 
-    private var binding:ActivityBaseBinding? =null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityBaseBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
     }
 
-    fun showProgressDialog(text: String){
+    /**
+     * This function is used to show the progress dialog with the title and message to user.
+     */
+    fun showProgressDialog(text: String) {
+        mProgressDialog = Dialog(this)
+
+        /*Set the screen content from a layout resource.
+        The resource will be inflated, adding all top-level views to the screen.*/
         mProgressDialog.setContentView(R.layout.dialog_progress)
-        mProgressDialog.findViewById<TextView>(R.id.tv_progress_text).text = text
-        mProgressDialog.show()
 
+        mProgressDialog.tv_progress_text.text = text
+
+        //Start the dialog and display it on screen.
+        mProgressDialog.show()
     }
 
-    fun hideProgressDialog(){
+    /**
+     * This function is used to dismiss the progress dialog if it is visible to user.
+     */
+    fun hideProgressDialog() {
         mProgressDialog.dismiss()
     }
 
-
-
-    fun getCurrentUserId(): String{
+    fun getCurrentUserID(): String {
         return FirebaseAuth.getInstance().currentUser!!.uid
     }
 
-    fun doubleBackToExit(){
-        if(doubleBackToExitPressedOnce){
+    fun doubleBackToExit() {
+        if (doubleBackToExitPressedOnce) {
             super.onBackPressed()
             return
         }
-        
+
         this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, "Back Press Again To Exit", Toast.LENGTH_SHORT).show()
+        Toast.makeText(
+                this,
+                resources.getString(R.string.please_click_back_again_to_exit),
+                Toast.LENGTH_SHORT
+        ).show()
 
-        Handler().postDelayed({doubleBackToExitPressedOnce = false},2000)
+        Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
     }
 
-    fun showErrorSnackBar(message:String){
-        val snackbar = Snackbar.make(findViewById(android.R.id.content),
-            message,Snackbar.LENGTH_LONG)
-        val snackbarView = snackbar.view
-        snackbarView.setBackgroundColor(ContextCompat.getColor(this,
-            R.color.snackbar_error_color))
-        snackbar.show()
-
+    fun showErrorSnackBar(message: String) {
+        val snackBar =
+                Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                        this@BaseActivity,
+                        R.color.snackbar_error_color
+                )
+        )
+        snackBar.show()
     }
-
 }
+// END
--- app/src/main/java/com/gautamtuteja/trellone/activities/SignInActivity.kt
+++ app/src/main/java/com/gautamtuteja/trellone/activities/SignInActivity.kt
@@ -1,40 +1,107 @@
-package com.gautamtuteja.trellone.activities
+package com.projemanag.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
-import com.gautamtuteja.trellone.R
-import com.gautamtuteja.trellone.databinding.ActivitySignInBinding
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.projemanag.R
+import com.projemanag.firebase.FirestoreClass
+import com.projemanag.model.User
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
-class SignInActivity : AppCompatActivity() {
-
-    private var binding : ActivitySignInBinding? = null
-
+class SignInActivity : BaseActivity() {
+    /**
+     * This function is auto created by Android when the Activity Class is created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_sign_in)
 
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        SetUpActionBar()
+        setupActionBar()
 
+        btn_sign_in.setOnClickListener {
+            signInRegisteredUser()
+        }
     }
 
-    private fun SetUpActionBar(){
+    /**
+     * A function for actionBar Setup.
+     */
+    private fun setupActionBar() {
 
-        setSupportActionBar(binding?.toolbarSignInActivity)
+        setSupportActionBar(toolbar_sign_in_activity)
 
         val actionBar = supportActionBar
-        if (actionBar!=null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
 
-        binding?.toolbarSignInActivity?.setNavigationOnClickListener { onBackPressed() }
+        toolbar_sign_in_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = et_email.text.toString().trim { it <= ' ' }
+        val password: String = et_password.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Calling the FirestoreClass signInUser function to get the data of user from database.
+                        FirestoreClass().loadUserData(this@SignInActivity)
+                    } else {
+                        Toast.makeText(
+                            this@SignInActivity,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    /**
+     * A function to validate the entries of a user.
+     */
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
+    }
+
+    /**
+     * A function to get the user details from the firestore database after authentication.
+     */
+    fun signInSuccess(user: User) {
+
+        hideProgressDialog()
+
+        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+        this.finish()
+    }
 }
